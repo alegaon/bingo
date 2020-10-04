34a052cde0ffb23b1794bc32ba031b17f94bd157
september <alegaon@outlook.com>
2020-10-04 20:23:33 -0300

    test de logicas

--- js/js_02_bolillero.js
+++ js/js_02_bolillero.js
@@ -1,1 +1,19 @@
-console.log("desde js_02: esto seria el bolillero")
+console.log("desde js_02: esto seria el bolillero");
+
+// definir rango de numeros
+let = [];
+function definir_rango(desde, hasta){
+
+
+    return resultado;
+
+};
+
+console.log(definir_rango)
+
+function myFunction() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "number");
+    x.setAttribute("value", "12345");
+    document.body.appendChild(x);
+  }
